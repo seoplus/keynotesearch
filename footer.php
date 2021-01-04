de5082ddb44b0f218120a32b0f2036d8bb0ad798
efenny <e.freundorfer@gmail.com>
2021-01-04 15:17:48 -0500

    added in linkedin insight tag in the footer

--- footer.php
+++ footer.php
@@ -68,6 +68,27 @@ defined('ABSPATH') || exit;
 
 <?php wp_footer(); ?>
 
+<!-- Linkedin insight tag -->
+<script type="text/javascript">
+_linkedin_partner_id = "979140";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script>
+<script type="text/javascript">
+(function() {
+  var s = document.getElementsByTagName("script")[0];
+  var b = document.createElement("script");
+  b.type = "text/javascript";
+  b.async = true;
+  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+  s.parentNode.insertBefore(b, s);
+})();
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none;" alt=""
+    src="https://px.ads.linkedin.com/collect/?pid=979140&fmt=gif" />
+</noscript>
+<!-- end Linkedin insight tag -->
 </body>
 
 </html>
